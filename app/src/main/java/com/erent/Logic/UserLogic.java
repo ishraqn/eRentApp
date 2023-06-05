829f9dd6966b2c9b57439086981b15a6fdd77789
Ishraq Md Nazrul <nazrulim@myumanitoba.ca>
2023-06-05 16:31:26 -0500

    Resolved the issues

---
+++ app/src/main/java/com/erent/Logic/UserLogic.java
@@ -0,0 +1,29 @@
+package com.erent.logic;
+
+import com.erent.objects.User;
+import com.erent.persistence.IUserPersistence;
+
+public class UserLogic implements IUserLogic{
+    private IUserPersistence userDB;
+
+    public UserLogic(IUserPersistence database)
+    {
+        this.userDB = database;
+    }
+
+    public User createNewUser(String username)
+    {
+        return userDB.createUser(username);
+    }
+
+    public boolean deleteUser(int userID)
+    {
+        return userDB.deleteUser(userID);
+    }
+
+    public boolean authenticateUser(int userID,String username)
+    {
+        User user = userDB.getUserByID(userID);
+        return user.getUsername().equals(username);
+    }
+}
