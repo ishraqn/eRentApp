7df578883426914f8b81dded1230bc5026297b99
Ishraq Md Nazrul <nazrulim@myumanitoba.ca>
2023-06-04 23:26:54 -0500

    Added the logic layer ability for creation/deletion,auth of users

--- app/src/main/java/com/erent/Logic/IUserLogic.java
+++ app/src/main/java/com/erent/Logic/IUserLogic.java
@@ -1,2 +1,10 @@
-package com.erent.Logic;public interface IUserLogic {
+package com.erent.Logic;
+
+import com.erent.objects.User;
+
+public interface IUserLogic
+{
+    boolean createNewUser(String UserName);
+    boolean deleteUser(int ID);
+    boolean userAuth(int ID,String UserName);
 }
--- app/src/main/java/com/erent/Logic/logicStubs/UserLogic.java
+++ app/src/main/java/com/erent/Logic/logicStubs/UserLogic.java
@@ -1,2 +1,44 @@
-package com.erent.Logic.logicStubs;public class UserLogic {
+package com.erent.Logic.logicStubs;
+
+import com.erent.Logic.IUserLogic;
+import com.erent.objects.User;
+import com.erent.persistence.IUserPersistence;
+
+
+public class UserLogic implements IUserLogic {
+    private IUserPersistence UserDB;
+
+    public UserLogic(IUserPersistence database)
+    {
+        this.UserDB = database;
+    }
+
+    public boolean createNewUser(String UserName)
+    {
+        User newUser = UserDB.createUser(UserName);
+        if(newUser != null)
+        {
+            System.out.println("Welcome New User " + UserName);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteUser(int ID)
+    {
+        boolean deletedUser = UserDB.deleteUser(ID);
+        return deletedUser;
+    }
+
+    public boolean userAuth(int ID,String UserName)
+    {
+        User userID = UserDB.getUserByID(ID);
+        User userName = UserDB.getUserByUsername(UserName);
+
+        if (userID.getUserID() == ID && userName.getUsername() == UserName)
+        {
+            return true;
+        }
+        return false;
+    }
 }
