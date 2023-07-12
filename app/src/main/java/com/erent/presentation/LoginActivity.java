fe3a227af714e5f9c9aa202a23d1da495278048a
Kaiden Bereznycky <bereznyk@myumanitoba.ca>
2023-07-12 20:21:47 +0000

    Merge kaiden-account-signup into dev

--- app/src/main/java/com/erent/presentation/LoginActivity.java
+++ app/src/main/java/com/erent/presentation/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.erent.presentation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.erent.R;
 
+import com.erent.logic.IUserLogic;
+import com.erent.logic.UserLogic;
+import com.erent.objects.User;
+import com.erent.persistence.stubs.UserPersistence;
 import com.erent.persistence.utils.DBHelper;
+
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -24,8 +30,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private IUserLogic uLogic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +46,36 @@ public class LoginActivity extends AppCompatActivity {
 
         DBHelper.copyDatabaseToDevice(this);
 
+        uLogic = new UserLogic(new UserPersistence());
+    }
+
+    public void login(View view) {
+        EditText usernameField;
+        EditText passwordField;
+        String username;
+        String password;
+
+        /* get the fields the text is typed into */
+        usernameField = (EditText) findViewById(R.id.inputButtonUsername);
+        passwordField = (EditText) findViewById(R.id.inputButtonPassword);
+
+        /* get the text from the fields */
+        username = usernameField.getText().toString();
+        password = passwordField.getText().toString();
+
+        if(uLogic.authenticateUser(username)) {
+            /* Insert code to store logged in user */
+
+            /* Switch to the homepage once logged in */
+            Intent switchActivityIntent = new Intent(this, MainActivity.class);
+            startActivity(switchActivityIntent);
+        } else {
+            /*do nothing right now*/
+        }
+    }
+
+    public void goToSignUp(View view) {
+        Intent switchActivityIntent = new Intent(this, SignUpActivity.class);
+        startActivity(switchActivityIntent);
     }
 }
